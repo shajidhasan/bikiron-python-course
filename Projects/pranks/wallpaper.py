247a3366e7387d85d67ac214a7dd65e6c6b98637
shajidhasan <shajid6063@gmail.com>
2021-12-12 14:39:49 +0600

    update

--- Projects/pranks/wallpaper.py
+++ Projects/pranks/wallpaper.py
@@ -1,6 +1,7 @@
 import ctypes
+from os.path import abspath
 from time import sleep
 
 while True:
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, "wallpaper.png" , 0)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, abspath("wallpaper.png"), 0)
     sleep(50)
